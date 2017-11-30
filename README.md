a078c6884b9602b93beb416a7501fdc54010bd7b
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-11-30 17:02:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# A simple node server used for RESTful API testing etc.
+- Deployed to Heroku
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send({hello: 'world!'});
+});
+
+const PORT = 8080;
+
+server.listen(PORT,() => {
+  console.log(`Server is up on port ${PORT}`);
+});
